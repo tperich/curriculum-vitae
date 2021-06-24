3db14d2495fb5d2a75aa53beced7fe40234782c0
Tomislav Peric <toca91@gmail.com>
2021-06-24 20:23:17 +0200

    Add JSON generation script

---
+++ bin/prepare-json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import json
+import glob
+from pathlib import Path
+
+workdir = Path().absolute().name
+targetdir = f"{workdir}/src/data"
+
+result = []
+for f in glob.glob(f"../{targetdir}/*.json"):
+    with open(f, "rb") as infile:
+        filename = Path(f).name
+        filename_base = Path(filename).stem
+        data = json.load(infile)
+        result.append({filename_base: data})
+
+with open("public/cv.json", "w") as f:
+    encoded = json.dumps(result, indent=True)
+    f.write(encoded)
